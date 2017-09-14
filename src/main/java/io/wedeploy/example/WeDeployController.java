1656124ad11afb6ecc0e3c29801b83e49873431c
ClaudioElio <31865946+ClaudioElio@users.noreply.github.com>
2017-09-14 12:29:51 +0200

    Update WeDeployController.java

--- src/main/java/io/wedeploy/example/WeDeployController.java
+++ src/main/java/io/wedeploy/example/WeDeployController.java
@@ -5,13 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/hello-2")
 public class WeDeployController {
 
     public WeDeployController() {
 
     }
 
-    @RequestMapping("/hello-2")
     public ModelAndView hello() {
         return new ModelAndView("layout");
     }
