8ba2447a8dc17f07f65177e8ced83e737ad2ac07
ClaudioElio <claudio.elio.mariani@gmail.com>
2017-11-11 14:55:51 +0100

    modified:   src/main/java/it/mariani/titti/main/TittiMain.java

--- src/main/java/it/mariani/titti/main/TittiMain.java
+++ src/main/java/it/mariani/titti/main/TittiMain.java
@@ -2,8 +2,10 @@ package it.mariani.titti.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication(scanBasePackages={"it.mariani.titti.controller"})
+@EnableOAuth2Sso
 public class TittiMain {
 
 	public static void main(String[] args) {
