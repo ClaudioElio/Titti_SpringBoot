fafc13d9e97be4df9c5cd01ce069a7788c7d692c
ClaudioElio <31865946+ClaudioElio@users.noreply.github.com>
2018-04-08 23:49:51 +0200

    Update TittiMain.java

--- src/main/java/it/mariani/titti/main/TittiMain.java
+++ src/main/java/it/mariani/titti/main/TittiMain.java
@@ -3,7 +3,7 @@ package it.mariani.titti.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(scanBasePackages={"it.mariani.titti.controller"})
 public class TittiMain {
