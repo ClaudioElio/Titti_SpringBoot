ef502a165410678d843230849bc92d89d5b11cc3
ClaudioElio <31865946+ClaudioElio@users.noreply.github.com>
2018-04-09 16:53:15 +0200

    Update and rename UserController2.java to UserController.java

--- src/main/java/it/mariani/titti/controller/UserController.java
+++ src/main/java/it/mariani/titti/controller/UserController.java
@@ -6,11 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserController2 {
+public class UserController {
 
     @RequestMapping("/user/me")
     public Principal user(Principal principal) {
         System.out.println(principal);
         return principal;
     }
+
 }
