111576c271adb4627fa6ecf6a40a7ef2b16c0a9c
ClaudioElio <31865946+ClaudioElio@users.noreply.github.com>
2018-04-09 16:25:35 +0200

    Update UserController.java

--- src/main/java/it/mariani/titti/controller/UserController.java
+++ src/main/java/it/mariani/titti/controller/UserController.java
@@ -3,6 +3,7 @@ package it.mariani.titti.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/ctrl-user")
