bf116b608fd1d58b73ee7d4cb1b187e9a95f2fae
ClaudioElio <31865946+ClaudioElio@users.noreply.github.com>
2018-04-09 16:45:16 +0200

    Create UserController2.java

---
+++ src/main/java/it/mariani/titti/controller/UserController2.java
@@ -0,0 +1,16 @@
+package it.mariani.titti.controller;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController2 {
+
+    @RequestMapping("/user/me")
+    public Principal user(Principal principal) {
+        System.out.println(principal);
+        return principal;
+    }
+}
