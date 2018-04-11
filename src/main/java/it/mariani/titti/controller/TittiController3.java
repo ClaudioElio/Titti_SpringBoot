f8040157da0d215b069defa3bd6ffc2b2f68cd22
ClaudioElio <31865946+ClaudioElio@users.noreply.github.com>
2018-04-11 19:01:14 +0200

    Update TittiController3.java

--- src/main/java/it/mariani/titti/controller/TittiController3.java
+++ src/main/java/it/mariani/titti/controller/TittiController3.java
@@ -1,11 +1,11 @@
 package it.mariani.titti.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ctrl-3")
