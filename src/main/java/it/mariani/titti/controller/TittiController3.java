fe23e1dc20de42c06a04e82dffa266d0b750b5d2
ClaudioElio <31865946+ClaudioElio@users.noreply.github.com>
2018-04-11 19:01:52 +0200

    Update TittiController3.java

--- src/main/java/it/mariani/titti/controller/TittiController3.java
+++ src/main/java/it/mariani/titti/controller/TittiController3.java
@@ -14,7 +14,7 @@ public class TittiController3 {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping(value="/hello-3")
+    @RequestMapping(value = "/hello-3")
     public @ResponseBody Greeting sayHello(@RequestParam(value="name", required=false, defaultValue="Stranger") String name, @RequestParam(value="number", required=false, defaultValue="-1") String num) {
 	    long greetingNum = num.equals("-1") ? counter.incrementAndGet(): Long.parseLong(num, 10); 
         return new Greeting(greetingNum, String.format(template, name));
