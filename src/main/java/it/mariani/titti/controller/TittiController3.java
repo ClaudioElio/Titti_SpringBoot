0fe39bc7cd0fcdf533e6c0dc5c60a7485efff11b
ClaudioElio <31865946+ClaudioElio@users.noreply.github.com>
2018-04-13 23:11:42 +0200

    Update TittiController3.java

--- src/main/java/it/mariani/titti/controller/TittiController3.java
+++ src/main/java/it/mariani/titti/controller/TittiController3.java
@@ -2,8 +2,8 @@ package it.mariani.titti.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +15,8 @@ public class TittiController3 {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping(value = "/hello-3")
-    public @ResponseBody Greeting sayHello(@RequestParam(value = "name", required = false, defaultValue = "Stranger") String name, @RequestParam(value = "number", required = false, defaultValue = "-1") String num) {
+    public @ResponseBody Greeting sayHello(@RequestParam(value = "name", required = false, defaultValue = "Stranger") String name,
+                                           @RequestParam(value = "number", required = false, defaultValue = "-1") String num) {
  
         long greetingNum = num.equals("-1") ? counter.incrementAndGet() : Long.parseLong(num, 10); 
         return new Greeting(greetingNum, String.format(template, name));
