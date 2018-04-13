4a7f607649646b27eb90906be2696ef964669312
ClaudioElio <31865946+ClaudioElio@users.noreply.github.com>
2018-04-13 20:22:26 +0200

    Update UserController.java

--- src/main/java/it/mariani/titti/controller/UserController.java
+++ src/main/java/it/mariani/titti/controller/UserController.java
@@ -1,7 +1,8 @@
 package it.mariani.titti.controller;
 
+import it.mariani.titti.utility.Logger;
+import it.mariani.titti.utility.LoggerFactory;
 import java.security.Principal;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/ctrl-user")
 public class UserController {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     @RequestMapping(value = "/me")
     public Principal user(Principal principal) {
-        System.out.println(principal);
+        Logger.info("Principal:", principal);
         return principal;
     }
 
