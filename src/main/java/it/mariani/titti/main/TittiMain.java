225976ae49a0fc6a0375be1fa4eaa9bb85938d01
ClaudioElio <31865946+ClaudioElio@users.noreply.github.com>
2018-04-21 22:54:57 +0200

    Update TittiMain.java

--- src/main/java/it/mariani/titti/main/TittiMain.java
+++ src/main/java/it/mariani/titti/main/TittiMain.java
@@ -3,7 +3,7 @@ package it.mariani.titti.main;
 public class TittiMain {
 
     public static void main(String[] args) {
-        SpringApplication.run(TittiApplication.class, args);
+        TittiApplication.run(args);
     }
 
 }
