f771de6ea8936a1c6af53e98d7d556c180f83781
ClaudioElio.Mariani <ClaudioElio.Mariani@2110P024.gbm.lan>
2019-04-30 14:36:40 +0200

    modifica x servlet container

--- src/main/java/it/mariani/titti/main/TittiApplication.java
+++ src/main/java/it/mariani/titti/main/TittiApplication.java
@@ -2,9 +2,10 @@ package it.mariani.titti.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = {"it.mariani.titti.controller"})
-public class TittiApplication {
+public class TittiApplication extends SpringBootServletInitializer {
 
   public static void run(String[] args) {
     SpringApplication.run(TittiApplication.class, args);
