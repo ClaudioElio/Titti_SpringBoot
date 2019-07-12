24e46380bc44fa73275d9379569daf89cdbd744e
ClaudioElio.Mariani <ClaudioElio.Mariani@2110P024.gbm.lan>
2019-07-12 15:00:50 +0200

    con Eureka

--- src/main/java/it/mariani/titti/main/TittiApplication.java
+++ src/main/java/it/mariani/titti/main/TittiApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {"it.mariani.titti.controller"})
 public class TittiApplication extends SpringBootServletInitializer {
 
