6837586beeaf8c7cd98908aec26352c3ba4453cc
ClaudioElio.Mariani <ClaudioElio.Mariani@2110P024.gbm.lan>
2019-10-11 09:19:23 +0200

    aggiornamento refactoring docker

---
+++ Eureka-ServiceRegistry/src/main/java/it/mariani/claudio/registry/EurekaServiceregistryApplication.java
@@ -0,0 +1,14 @@
+package it.mariani.claudio.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServiceregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceregistryApplication.class, args);
+	}
+}
