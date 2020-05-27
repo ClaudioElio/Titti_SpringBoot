c7c2c50247644761ea3285689705d5950fa0817f
ClaudioMariani <claudio.elio.mariani@gmail.com>
2020-05-27 11:11:34 +0200

    pulizia

--- Eureka-ServiceRegistry/src/main/java/it/mariani/claudio/registry/EurekaServiceRegistryApplication.java
+++ Eureka-ServiceRegistry/src/main/java/it/mariani/claudio/registry/EurekaServiceRegistryApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaServiceregistryApplication {
+public class EurekaServiceRegistryApplication {
 
 	public static void main(String[] args) {
 //		new SpringApplicationBuilder(EurekaServiceregistryApplication.class).web(true).run(args);
-		SpringApplication.run(EurekaServiceregistryApplication.class, args);
+		SpringApplication.run(EurekaServiceRegistryApplication.class, args);
 	}
 }
