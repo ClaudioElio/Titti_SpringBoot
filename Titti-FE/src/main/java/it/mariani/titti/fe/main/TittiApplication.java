57c926be32318f9cbbad503911c156fe06d802d4
ClaudioMariani <claudio.elio.mariani@gmail.com>
2020-06-01 19:16:23 +0200

    log spring hystrix

--- Titti-FE/src/main/java/it/mariani/titti/fe/main/TittiApplication.java
+++ Titti-FE/src/main/java/it/mariani/titti/fe/main/TittiApplication.java
@@ -3,12 +3,13 @@ package it.mariani.titti.fe.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = {"it.mariani.titti.fe.proxy"})
+@EnableCircuitBreaker
 @SpringBootApplication(scanBasePackages = {"it.mariani.titti.fe.controller"})
 public class TittiApplication extends SpringBootServletInitializer {
 
