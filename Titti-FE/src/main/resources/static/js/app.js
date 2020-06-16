7daf4d1a8a6f04bf5b2856cfb048bbfc46747038
ClaudioMariani <claudio.elio.mariani@gmail.com>
2020-06-16 23:27:39 +0200

    add vue

--- Titti-FE/src/main/resources/static/js/app.js
+++ Titti-FE/src/main/resources/static/js/app.js
@@ -1,6 +1,22 @@
-/**
- * 
- */
+const axiosConfig = {
+	headers: {
+		'Access-Control-Allow-Origin': '*',
+//			'Access-Control-Allow-Credentials': 'false',
+//			'Access-Control-Allow-Headers': 'Server,Connection,X-CorrelationID,Cache-Control,Date,Expires,Pragma,Server,Content-Type',
+//			'Access-Control-Allow-Methods': 'GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE, PATCH',
+//			'Access-Control-Expose-Headers': 'Server, Connection, X-CorrelationID, Cache-Control, Date, Expires, Pragma, Server, Content-Type',
+		'content-Type': 'application/json',
+//			"Accept": "*/*",
+//			"Cache-Control": "no-cache",
+		'Authorization': 'Basic YWRtaW46QWRtQkBuY28=',
+//			'Sec-Fetch-Site':	'cross-site',
+//			'Sec-Fetch-Mode':	'cors'
+		}
+//		credentials: "same-origin"
+};
+
+Vue.use(VueMaterial.default);
+
 const vm = new Vue({
   el: '#app',
   data: {
@@ -12,5 +28,12 @@ const vm = new Vue({
       {text: "Hello 3", url: "ctrl-3/hello-3"},
       {text: "User me", url: "ctrl-user/me"}
     ]
+  },
+  
+  methods: {
+		service_load: function() {
+			axios.get('http://claudj.ns0.it/titti/ctrl-3/hello-3', axiosConfig)
+				.then(response => (this.services = response.data.result.services))
+		}
   }
 });
